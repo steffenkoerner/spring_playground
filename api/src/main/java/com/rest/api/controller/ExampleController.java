0fdd4206df48a9f1a84877a0af1082f18a08085e
Steffen Koerner <>
2023-06-10 21:47:34 +0200

    Adding First REST Examples

---
+++ api/src/main/java/com/rest/api/controller/ExampleController.java
@@ -0,0 +1,66 @@
+package com.rest.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ExampleController {
+
+    /**
+     * This function handles calls to /api and /api/{name}. Could also be done with a separate controller.
+     * Then the if/else statements are not needed.
+     */
+    @GetMapping(value = { "/hello", "/hello/{name}" })
+    public ResponseTransfer SayHelloWithName(@PathVariable(required = false) String name){
+
+      if(name == null)
+          return new ResponseTransfer("Hello");
+      else
+          return new ResponseTransfer("Hello " + name);
+    }
+
+    @GetMapping(value = "/content", produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseBody
+
+    public ResponseTransfer postResponseXmlContent() {
+        return new ResponseTransfer("XML Content!");
+    }
+
+    @GetMapping(value = "/content", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseTransfer postResponseJsonContent() {
+        return new ResponseTransfer("JSON Content!");
+    }
+
+    @PostMapping
+    public ResponseTransfer SayHelloWith() {
+        return new ResponseTransfer ("");
+    }
+
+    // TODO: Send json object with error code back instead of html page
+    // Implement some post/put/delete rest handler
+
+    public class ResponseTransfer {
+        private String text;
+
+        ResponseTransfer(String text) {
+            this.text = text;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public class BadRequestException extends RuntimeException {
+    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public class ResourceNotFoundException extends RuntimeException {
+    }
+}
